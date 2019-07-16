ed584cdbb519ee4e66810e89731a23d4ff3eb5a7
JessyisKing <jesus.a.estefo@outlook.com>
2019-07-16 09:44:35 -0700

    add comment for class description

---
+++ SingASong/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingASong
+{
+    class Program
+    {
+        static void SingaSong(string title)
+        {
+            Console.WriteLine($"Lets sing {title}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SingaSong("Baby Shark!");
+        }
+    }
+}
